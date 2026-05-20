3f46b0336ff872dc111035827acfee6b20e31fac
Letícia Schütze <the.leticia.schutze@gmail.com>
2026-05-20 18:08:15 -0300

    Retirada de pastas extras

--- semestre-02/back-end/260220/js/externo.js
+++
@@ -1,5 +0,0 @@
-const nome = "Letícia Schütze";
-let idade = 20;
-let cidade = "Jaraguá do Sul";
-alert("Usando agora  JS externo");
-alert(`Oie eu sou a ${nome} moro em ${cidade} e tenho ${idade} anos`);
--- semestre-02/banco-de-dados/260331/lojaonline/global/header.php
+++
@@ -1,9 +0,0 @@
-    <header>
-        <nav>
-            <a href="index.php">Home</a>
-            <a href="produtos.php">Produtos</a>
-            <!-- NOVO PRODUTO -->
-            <!-- EDITAR PRODUTO -->
-            <a href="entrar.php">Sair</a>
-        </nav>
-    </header>
